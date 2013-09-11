0573bbf81adf85baba99823bf296277fd586b031
jgbs <joaogabrieldebrittoesilva@gmail.com>
2013-09-11 16:00:48 -0300

    refatoracao

--- src/milliwatt/controller/DepartamentoController.java
+++ src/milliwatt/controller/DepartamentoController.java
@@ -9,89 +9,80 @@ import milliwatt.utils.Global;
 
 public class DepartamentoController {
 
-	/*
-1
-Darcy Ribeiro
-https://matriculaweb.unb.br/matriculaweb/graduacao/oferta_dep.aspx?cod=1
-2
-Planaltina
-https://matriculaweb.unb.br/matriculaweb/graduacao/oferta_dep.aspx?cod=2
-3
-Ceilândia
-https://matriculaweb.unb.br/matriculaweb/graduacao/oferta_dep.aspx?cod=3
-4
-Gama
-https://matriculaweb.unb.br/matriculaweb/graduacao/oferta_dep.aspx?cod=4
-*/
-	
-	public static ArrayList<Departamento> getDepartmentList(){
-		
-		String urlName;
-		ArrayList<Campus> campusList = CampusController.getCampusList();
-		Campus c = campusList.get(3);//Recebe o objeto Campus que representa a FGA
+	public static ArrayList<Departamento> getDepartmentList(ArrayList<Campus> campusList, String id){
 		
 		ArrayList<Departamento> departamentoList = new ArrayList<Departamento>();
+		Campus c = null;
 		
-		HttpsPage site = new HttpsPage(c.getUrlName());
+		for(int i = 0; i < campusList.size(); i++ ){
+			if(campusList.get(i).getId().equals(id)) 
+				 c = campusList.get(i);//Recebe o objeto Campus que representa a FGA
+		}
+		
+		String urlName_departamento = c.getUrlName();
+		
+		//System.out.println(urlName_departamento);
+		HttpsPage site = new HttpsPage(urlName_departamento);
+		
 		site.connect();
 		
 		String htmlString = site.getOnlyHTML();
 		
-		//System.out.println(htmlString);
-		
-		String id_departamento="";
-		String sigla_departamento="";
+		String id_departamento = "";
+		String sigla_departamento = "";
+		String sigla_departamento_invertida = "";
 		String nome_departamento = "";
-		String urlName_departamento="";
-		Campus campus = null;
-		int index =0;
+		Departamento departamento = null;
+		int index = 0;
 		
 		while(index !=-1){
 			
 			index = htmlString.indexOf(Global.MW_DEPARTAMENT_ID);
-			//System.out.println(htmlString.charAt(index+20));
+			index = index - 34;
 			
-			while (htmlString.charAt(index-37)!='>'){
+			while (htmlString.charAt(index)!='>'){
+				sigla_departamento += htmlString.charAt(index);
 				index--;
 			}
 			
-			while (htmlString.charAt(index)!='<'){
-				sigla_departamento += htmlString.charAt(index);
-				index++;
-			}
-			System.out.println(sigla_departamento);
-			index = htmlString.indexOf(Global.MW_DEPARTAMENT_ID);
+			//FALTA INVERTER A SIGLA AQUI
+			
+			index = htmlString.indexOf(Global.MW_DEPARTAMENT_ID)+4;
 			
 			while (htmlString.charAt(index)!='>'){
 				id_departamento += htmlString.charAt(index);
 				index++;
 			}
+			
 			index++;
 			
 			while (htmlString.charAt(index)!='<'){
 				nome_departamento += htmlString.charAt(index);
 				index++;
 			}
+			
 			index++;
-			/*
-			urlName_campus = Global.MW_DEPARTMENT + id_campus;
 			
-			System.out.println(id_campus);
-			System.out.println(nome_campus);
-			System.out.println(urlName_campus);
+			System.out.println(id_departamento);
+			System.out.println(nome_departamento);
+			System.out.println(sigla_departamento_invertida);
+			System.out.println(urlName_departamento);
 			
-			htmlString=htmlString.substring(index);// Parte a String
-			index = htmlString.indexOf(Global.MW_CAMPUS_ID); // se for -1 PARA
 			
-			campus = new Campus(id_campus, nome_campus, urlName_campus);
-			CampusList.add(campus);
+			htmlString = htmlString.substring(index);// Parte a String
+			index = htmlString.indexOf(Global.MW_DEPARTAMENT_ID); // se for -1 PARA
 			
-			id_campus="";
-			nome_campus="";
-			urlName_campus="";*/
+			departamento = new Departamento(id_departamento, nome_departamento, sigla_departamento_invertida, urlName_departamento);
+			departamentoList.add(departamento);
+			
+			id_departamento="";
+			nome_departamento="";
+			urlName_departamento="";
 		}
-		site.disconnect();	
-		//return departamentoList;
-		return null;
+		
+		site.disconnect();
+		
+		return departamentoList;
+		//return null;
 	}
 }
