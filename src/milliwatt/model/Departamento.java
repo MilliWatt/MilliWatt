0f7f7fed4ee98a4265760b254702bc9ddf407e54
levimoraes <levi.moraes@yahoo.com.br>
2013-09-11 10:34:09 -0300

    Criacao da classe departamento na Model

--- src/milliwatt/model/Departamento.java
+++ src/milliwatt/model/Departamento.java
@@ -1,5 +1,62 @@
 package milliwatt.model;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Departamento {
 
+	private int codigo;
+	private String nome;
+	private String urlName;
+	private URL url;
+	
+	
+	public Departamento(int codigo, String nome, String urlName, URL url) {
+		
+		this.codigo = codigo;	
+		this.nome = nome;
+		this.urlName = urlName;
+		this.url = url;
+		try {
+			this.url = new URL(urlName);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUrlName() {
+		return urlName;
+	}
+
+	public void setUrlName(String urlName) {
+		this.urlName = urlName;
+	}
+
+	public URL getUrl() {
+		return url;
+	}
+
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+
+	
+	
 }
