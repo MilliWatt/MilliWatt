f9bf50e85f2398a20324ff355c6fb769507cc53e
levimoraes <levi.moraes@yahoo.com.br>
2013-09-11 12:16:07 -0300

    Refatoracao da classe global

---
+++ src/milliwatt/utils/Global.java
@@ -0,0 +1,30 @@
+package milliwatt.utils;
+
+public class Global {
+	//sempre termina com BARRA
+	public static final String KEY_DIR = "./KEY/";
+	public static final String HTML_DIR = "./HTML/";
+	public static final String MW_HOME = "https://matriculaweb.unb.br/matriculaweb/";
+	
+	
+	public static final String MW_GRADUATE = MW_HOME + "graduacao/";
+	public static final String MW_POSTGRADUATE = MW_HOME + "posgraduacao/";
+	
+	
+	public static final String MW_CAMPUS = MW_GRADUATE + "oferta_campus.aspx";//obter lista de campus
+	public static final String MW_CAMPUS_ID = "cod=";//utilizado para parse de campus
+	public static final String MW_DEPARTAMENT_ID = "cod=";
+	
+	//cod de 1 a 4 sendo 4 o Gama
+	public static final String MW_DEPARTMENT = MW_GRADUATE+"oferta_dep.aspx?cod="; //obter lista de departamentos e seus codigos.
+	
+	public static final String MW_DISCIPLINE =  MW_GRADUATE + "oferta_dis.aspx?cod=";//obter lista de disciplinas e seus codigos.
+	
+	// obter lista de turmas e informações de horarios e professores e etc
+	public static final String MW_CLASS = MW_GRADUATE + "oferta_dados.aspx?cod=" +/*CODIGO DISCI*/ "&dep=";/*COD_DEP */ 	
+	
+	
+	
+	
+	
+}
