2dbb43a107e7e2643c7bc7b3eb1bf2f6b3a033cb
levimoraes <levi.moraes@yahoo.com.br>
2013-09-13 15:56:21 -0300

    Teste na Main

--- src/milliwatt/main/MainTeste.java
+++ src/milliwatt/main/MainTeste.java
@@ -13,42 +13,42 @@ import milliwatt.model.Departamento;
 public class MainTeste {
 	public static void main(String args[]){
 
-//		Page p =  Page.getPage("http:\\www.google.com");		
-//		System.out.println(p.toString());
+		//Page p =  Page.getPage("http:\\www.google.com");		
+		//System.out.println(p.toString());
 
 		ArrayList<Campus> campusList = CampusController.getCampusList();
-		DepartamentoController.getDepartmentList(campusList,"4");	
-
-
 		ArrayList<Departamento> departamentoList = DepartamentoController.getDepartmentList(campusList, "1");
-
 		DisciplinaController.getDisciplinasList(departamentoList);	
+		
+		/*
+		File file = new File("./teste");
+		
+		
+		BufferedReader in = null;
+		try {
+			 in = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String inputLine, aux="";
+		try{
+		System.out.println(in.readLine());
+				while ((inputLine = in.readLine()) != null) {
+					inputLine = inputLine + "\n";
+		            // Imprime página no console
+		            //System.out.println(inputLine);//DEBUG
+		            // Grava pagina no arquivo
+		            aux = aux+inputLine;
+		        }
+		}catch(IOException e){e.printStackTrace();}
+		//System.out.println(aux);
+		//System.out.println(aux.charAt(aux2));
+		*/
+
 		
 	}
 
-	/*
-	File file = new File("./teste");
 	
-	
-	BufferedReader in = null;
-	try {
-		 in = new BufferedReader(new FileReader(file));
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	}
-	String inputLine, aux="";
-	try{
-	System.out.println(in.readLine());
-			while ((inputLine = in.readLine()) != null) {
-				inputLine = inputLine + "\n";
-	            // Imprime página no console
-	            //System.out.println(inputLine);//DEBUG
-	            // Grava pagina no arquivo
-	            aux = aux+inputLine;
-	        }
-	}catch(IOException e){e.printStackTrace();}
-	//System.out.println(aux);
-	//System.out.println(aux.charAt(aux2));
 */
 
 }
