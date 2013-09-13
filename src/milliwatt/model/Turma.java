4f854afdc101de70925e4170e848a31a6ed2bf1e
levimoraes <levi.moraes@yahoo.com.br>
2013-09-13 15:57:38 -0300

    Criacao de variaveis na modelo /Turma

--- src/milliwatt/model/Turma.java
+++ src/milliwatt/model/Turma.java
@@ -7,18 +7,58 @@ public class Turma {
 	private String identificador;
 	private String horarioInicioTurma;
 	private String horarioFimTurma;
+	private int total_vagas;
+	private int vagas_disponiveis;
+	private int vagas_ocupadas;
 	private ArrayList<String> diasTurma;
+	private ArrayList<Professor> listaProfessores;
 	
 	public Turma(){}
 
-	public Turma(String identificador, String horarioInicioTurma,
-			String horarioFimTurma, ArrayList<String> diasTurma) {
+	public Turma(String identificador, String horarioInicioTurma,String horarioFimTurma, int total_vagas, 
+			int vagas_disponiveis,int vagas_ocupadas, ArrayList<String> diasTurma,ArrayList<Professor> listaProfessores) {
 		this.identificador = identificador;
 		this.horarioInicioTurma = horarioInicioTurma;
 		this.horarioFimTurma = horarioFimTurma;
+		this.total_vagas = total_vagas;
+		this.vagas_disponiveis = vagas_disponiveis;
+		this.vagas_ocupadas = vagas_ocupadas;
 		this.diasTurma = diasTurma;
+		this.listaProfessores = listaProfessores;
 	}
 
+	public int getTotal_vagas() {
+		return total_vagas;
+	}
+
+	public void setTotal_vagas(int total_vagas) {
+		this.total_vagas = total_vagas;
+	}
+
+	public int getVagas_disponiveis() {
+		return vagas_disponiveis;
+	}
+
+	public void setVagas_disponiveis(int vagas_disponiveis) {
+		this.vagas_disponiveis = vagas_disponiveis;
+	}
+
+	public int getVagas_ocupadas() {
+		return vagas_ocupadas;
+	}
+
+	public void setVagas_ocupadas(int vagas_ocupadas) {
+		this.vagas_ocupadas = vagas_ocupadas;
+	}
+
+	public ArrayList<Professor> getListaProfessores() {
+		return listaProfessores;
+	}
+
+	public void setListaProfessores(ArrayList<Professor> listaProfessores) {
+		this.listaProfessores = listaProfessores;
+	}
+	
 	public String getIdentificador() {
 		return identificador;
 	}
