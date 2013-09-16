1dc0c4683fc08d9f4786accf3bf509caa4ad7e4d
jgbs <joaogabrieldebrittoesilva@gmail.com>
2013-09-16 01:23:27 -0300

    conclusao beta 1.1

--- src/milliwatt/main/MainTeste.java
+++ src/milliwatt/main/MainTeste.java
@@ -20,16 +20,6 @@ public class MainTeste {
 		//Page p =  Page.getPage("http:\\www.google.com");		
 		//System.out.println(p.toString());
 
-		ArrayList<Campus> campusList = CampusController.getCampusList();		
-
-		ArrayList<Departamento> departamentoList = DepartamentoController.getDepartmentList(campusList, "4");
-		campusList.get(3).setDepartamentoList(departamentoList);
-//		ArrayList<Disciplina> disciplinaList = DisciplinaController.getDisciplinaList(departamentoList, "650");
-//		TurmaController.getTurmaList(disciplinaList, "113034");
-
-		ArrayList<Departamento> departamentoList = DepartamentoController.getDepartmentList(campusList, "4");		
-		ArrayList<Disciplina> disciplinaList = DisciplinaController.getDisciplinaList(departamentoList, "650");
-		TurmaController.getTurmaList(disciplinaList, "193640");
 		String campusDesejado = JOptionPane.showInputDialog(null, "Informe o nome completo do Campus Desejado!",  
 				"Olá, Seja Bem Vindo(a)!", JOptionPane.INFORMATION_MESSAGE);
 		String siglaDepartamentoDesejado = JOptionPane.showInputDialog(null, "Informe a sigla do Departamento Desejado!",  
@@ -49,17 +39,15 @@ public class MainTeste {
 			JOptionPane.showMessageDialog(null, substrings);	
 		}
 		
-		CampusDAO.toFile(campusList);
-		ArrayList<Campus> load=  CampusDAO.toObject();
-		
-		System.out.println(load.get(3).getDepartamentoList().get(0).getSigla());
-		
 		ArrayList<Campus> campusList = CampusController.getCampusList();		
 		ArrayList<Departamento> departamentoList = DepartamentoController.getDepartmentList(campusList, campusDesejado);		
 		ArrayList<Disciplina> disciplinaList = DisciplinaController.getDisciplinaList(departamentoList, siglaDepartamentoDesejado);
 		ArrayList<Turma> turmaList = TurmaController.getTurmaList(disciplinaList, codigoDisciplinaDesejada);
 		
-
+		CampusDAO.toFile(campusList);
+		ArrayList<Campus> load=  CampusDAO.toObject();
+		
+		JOptionPane.showMessageDialog(null, load.get(3).getDepartamentoList().get(0).getSigla());
 		
 	}
 
